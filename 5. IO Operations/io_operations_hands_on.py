e3fe1f5fa1065905b8e4a44b209e054785c78f86
Priyanshu Namdev <154689360+Priyanshu1914@users.noreply.github.com>
2025-07-27 23:00:33 +0530

    Update io_operations_hands_on.py

--- 5. IO Operations/io_operations_hands_on.py
+++ 5. IO Operations/io_operations_hands_on.py
@@ -5,6 +5,7 @@ with open('sample.txt', 'r') as file:
     print(content)
 file.close()
 
+# --------------------------------------------------
 # Write a program to read first n lines from a txt file. Get n as user input.
 
 n = int(input("Enter the number of lines to read: "))
@@ -16,6 +17,7 @@ with open('sample.txt', 'r') as file:
         print(line.strip())
 file.close()
 
+# --------------------------------------------------
 # Write a program to accept input from user and append it to a txt file.
 
 user_input = input("Enter text to append to the file: ")
@@ -23,6 +25,7 @@ with open('sample.txt', 'a') as file:
     file.write(user_input + '\n')
 file.close()
 
+# --------------------------------------------------
 # Write a program to read contents from a txt file line by line and store each line into a list.
 
 with open('sample.txt', 'r') as file:
@@ -31,6 +34,7 @@ with open('sample.txt', 'r') as file:
     print(lines_list)
 file.close()
 
+# --------------------------------------------------
 # Write a program to find the longest word from the txt file contents, assuming that the file will have only one longest word in it.
 
 with open('sample.txt', 'r') as file:
@@ -39,6 +43,7 @@ with open('sample.txt', 'r') as file:
     print("Longest word:", longest_word)
 file.close()
 
+# --------------------------------------------------
 # Write a program to count the frequency of a user entered word in a txt file.
 
 user_word = input("Enter a word to count its frequency: ")
@@ -47,3 +52,5 @@ with open('sample.txt', 'r') as file:
     frequency = content.lower().split().count(user_word.lower())
     print(f"The word '{user_word}' appears {frequency} times in the file.")
 file.close()
+
+# --------------------------------------------------
