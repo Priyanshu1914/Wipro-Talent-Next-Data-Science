a10f3cf3c9bfb327d50f62bd6aef1280cff56dab
Priyanshu Namdev <154689360+Priyanshu1914@users.noreply.github.com>
2025-07-27 23:04:33 +0530

    Update dictionary_hands_on.py

--- 2. Data Structure/dictionary_hands_on.py
+++ 2. Data Structure/dictionary_hands_on.py
@@ -6,6 +6,8 @@ sample_dict = {0: 10, 1: 20}
 sample_dict[2] = 30
 print(sample_dict)
 
+
+# --------------------------------------------------
 # Write a program to concatenate the following dictionaries to create a new one.  
 # Sample Dictionary : 
 # dic1={1:10, 2:20}  dic2={3:30, 4:40}  dic3={5:50,6:60} 
@@ -17,6 +19,8 @@ dic3 = {5: 50, 6: 60}
 new_dict = {**dic1, **dic2, **dic3}
 print(new_dict)
 
+
+# --------------------------------------------------
 # Write a program to check if a given key already exists in a dictionary.
 
 sample_dict = {0: 10, 1: 20}
@@ -26,6 +30,8 @@ if key_to_check in sample_dict:
 else:
     print(f"Key {key_to_check} does not exist in the dictionary.")
 
+
+# --------------------------------------------------
 # Write a program to iterate over a dictionary using for loop and print the keys alone, values alone and both keys and values.
 
 sample_dict = {0: 10, 1: 20, 2: 30}
@@ -39,13 +45,21 @@ print("Keys and Values:")
 for key, value in sample_dict.items():
     print(f"{key}: {value}")
 
+
+# --------------------------------------------------
 # Write a program to prepare a dictionary where the keys are numbers between 1 and 15 (both included) and the values are square of the keys.
 
 sample_dict = {i: i**2 for i in range(1, 16)}
 print(sample_dict)
 
+
+# --------------------------------------------------
 # Write a program to sum all the values in a dictionary, considering the values will be of int type.
 
 sample_dict = {0: 10, 1: 20, 2: 30}
 total_sum = sum(sample_dict.values())
 print(f"Total sum of values: {total_sum}")
+
+
+
+# --------------------------------------------------
